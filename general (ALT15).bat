ecccaf700ef479e85b2d8523a926c86fe9242f06
korao <korao.fnc@gmail.com>
2026-08-08 20:05:36 +0300

    update

--- general (ALT15).bat
+++ general (ALT15).bat
@@ -9,13 +9,15 @@ call service.bat load_game_filter
 call service.bat load_user_lists
 echo:
 
-set "BIN=%~dp0bin\"
-set "LISTS=%~dp0lists\"
-cd /d %BIN%
+set "BIN=%~dp0bin"
+set "LISTS=%~dp0lists"
+
+cd /d "%BIN%"
 
 start "zapret: %~n0" /min "%BIN%winws.exe" ^
 --wf-tcp=80,443,2053,2083,2087,2096,8443,%GameFilterTCP% ^
---wf-udp=443,19294-19344,50000-50100,%GameFilterUDP% ^
+--wf-udp=443,19294-19344,50000-50200,%GameFilterUDP% ^
+
 
 --filter-udp=443 ^
 --hostlist="%LISTS%list-general.txt" ^
@@ -27,40 +29,53 @@ start "zapret: %~n0" /min "%BIN%winws.exe" ^
 --dpi-desync=fake ^
 --dpi-desync-repeats=11 ^
 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" ^
---new ^
 
+
+--new ^
 --filter-udp=19294-19344,50000-50100 ^
 --filter-l7=discord,stun ^
 --dpi-desync=fake ^
 --dpi-desync-fake-discord="%BIN%ACTIVE_DISCORD_UDP.bin" ^
 --dpi-desync-fake-stun="%BIN%ACTIVE_DISCORD_UDP.bin" ^
 --dpi-desync-repeats=2 ^
---new ^
 
---filter-tcp=2053,2083,2087,2096,2096,8443 ^
+
+--new ^
+--filter-udp=50101-50200 ^
+--filter-l7=discord,stun ^
+--dpi-desync=fake ^
+--dpi-desync-fake-discord="%BIN%ACTIVE_DISCORD_UDP.bin" ^
+--dpi-desync-fake-stun="%BIN%ACTIVE_DISCORD_UDP.bin" ^
+--dpi-desync-repeats=2 ^
+
+
+--new ^
+--filter-tcp=2053,2083,2087,2096,8443 ^
 --hostlist-domains=discord.media ^
 --dpi-desync=hostfakesplit ^
 --dpi-desync-fooling=ts ^
 --dpi-desync-repeats=2 ^
 --dpi-desync-hostfakesplit-mod=host=www.google.com ^
---new ^
 
+--new ^
 --filter-tcp=443 ^
 --hostlist="%LISTS%list-discord.txt" ^
 --dpi-desync=hostfakesplit ^
 --dpi-desync-fooling=ts ^
 --dpi-desync-repeats=2 ^
 --dpi-desync-hostfakesplit-mod=host=www.google.com ^
---new ^
 
+
+--new ^
 --filter-tcp=443 ^
 --hostlist="%LISTS%list-google.txt" ^
 --ip-id=zero ^
 --dpi-desync=hostfakesplit ^
 --dpi-desync-fooling=ts ^
 --dpi-desync-hostfakesplit-mod=host=www.google.com ^
---new ^
 
+
+--new ^
 --filter-tcp=443 ^
 --hostlist="%LISTS%list-osu.txt" ^
 --dpi-desync=fake,multisplit ^
@@ -71,16 +86,18 @@ start "zapret: %~n0" /min "%BIN%winws.exe" ^
 --dpi-desync-split-seqovl-pattern="%BIN%tls_clienthello_www_google_com.bin" ^
 --dpi-desync-fake-tls="%BIN%tls_clienthello_max_ru.bin" ^
 --dpi-desync-fake-http="%BIN%tls_clienthello_max_ru.bin" ^
---new ^
 
+
+--new ^
 --filter-udp=443 ^
 --hostlist="%LISTS%list-osu.txt" ^
 --dpi-desync=fake ^
 --dpi-desync-repeats=15 ^
 --dpi-desync-any-protocol=1 ^
 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" ^
---new ^
 
+
+--new ^
 --filter-tcp=80,443 ^
 --hostlist="%LISTS%list-osu.txt" ^
 --dpi-desync=fake,multisplit ^
@@ -91,9 +108,9 @@ start "zapret: %~n0" /min "%BIN%winws.exe" ^
 --dpi-desync-split-seqovl-pattern="%BIN%tls_clienthello_www_google_com.bin" ^
 --dpi-desync-fake-tls="%BIN%tls_clienthello_www_google_com.bin" ^
 --dpi-desync-fake-http="%BIN%tls_clienthello_www_google_com.bin" ^
+
+
 --new ^
-
-
 --filter-udp=443 ^
 --ipset="%LISTS%ipset-all.txt" ^
 --hostlist-exclude="%LISTS%list-osu.txt" ^
@@ -104,8 +121,9 @@ start "zapret: %~n0" /min "%BIN%winws.exe" ^
 --dpi-desync=fake ^
 --dpi-desync-repeats=11 ^
 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" ^
---new ^
 
+
+--new ^
 --filter-tcp=80,443,8443 ^
 --ipset="%LISTS%ipset-all.txt" ^
 --hostlist-exclude="%LISTS%list-osu.txt" ^
@@ -119,11 +137,11 @@ start "zapret: %~n0" /min "%BIN%winws.exe" ^
 --dpi-desync-fooling=ts ^
 --dpi-desync-repeats=8 ^
 --dpi-desync-split-seqovl-pattern="%BIN%tls_clienthello_max_ru.bin" ^
---dpi-desync-fake-tls="%BIN%stun.bin" ^
 --dpi-desync-fake-tls="%BIN%tls_clienthello_max_ru.bin" ^
 --dpi-desync-fake-http="%BIN%tls_clienthello_max_ru.bin" ^
---new ^
 
+
+--new ^
 --filter-tcp=%GameFilterTCP% ^
 --ipset="%LISTS%ipset-all.txt" ^
 --hostlist-exclude="%LISTS%list-osu.txt" ^
@@ -137,13 +155,11 @@ start "zapret: %~n0" /min "%BIN%winws.exe" ^
 --dpi-desync-fooling=ts ^
 --dpi-desync-repeats=8 ^
 --dpi-desync-split-seqovl-pattern="%BIN%tls_clienthello_max_ru.bin" ^
---dpi-desync-fake-tls="%BIN%stun.bin" ^
 --dpi-desync-fake-tls="%BIN%tls_clienthello_max_ru.bin" ^
 --dpi-desync-fake-http="%BIN%tls_clienthello_max_ru.bin" ^
+
+
 --new ^
-
-
-
 --filter-udp=%GameFilterUDP% ^
 --ipset="%LISTS%ipset-all.txt" ^
 --ipset-exclude="%LISTS%ipset-exclude.txt" ^
